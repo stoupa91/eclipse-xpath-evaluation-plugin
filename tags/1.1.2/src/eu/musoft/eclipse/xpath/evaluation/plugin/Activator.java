bf101d5565c348996751bca136eabc074dce7b84
stoupa91@gmail.com <stoupa91@gmail.com@9130e583-ed78-ccbd-5243-a5cfb3686d26>
2011-11-18 00:37:44 +0000

    Created branch 1.1.2

---
+++ tags/1.1.2/src/eu/musoft/eclipse/xpath/evaluation/plugin/Activator.java
@@ -0,0 +1,133 @@
+/**
+ * Copyright (c) 2011, Martin Uhlir
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * The name of the author may not be used to endorse or promote
+ *       products derived from this software without specific prior written
+ *       permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ''AS IS'' AND ANY EXPRESS
+ * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package eu.musoft.eclipse.xpath.evaluation.plugin;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.eclipse.core.runtime.FileLocator;
+import org.eclipse.core.runtime.ILog;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.BundleContext;
+
+/**
+ * The activator class controls the plug-in life cycle
+ */
+public class Activator extends AbstractUIPlugin {
+
+	// The plug-in ID
+	public static final String PLUGIN_ID = "eu.musoft.eclipse.xpath.evaluation.plugin"; //$NON-NLS-1$
+
+	// The shared instance
+	private static Activator plugin;
+	
+	private static ILog logger;
+
+	/**
+	 * The constructor
+	 */
+	public Activator() {
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext
+	 * )
+	 */
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+		plugin = this;
+		logger = this.getLog();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext
+	 * )
+	 */
+	public void stop(BundleContext context) throws Exception {
+		logger = null;
+		plugin = null;
+		super.stop(context);
+	}
+
+	/**
+	 * Returns the shared instance
+	 * 
+	 * @return the shared instance
+	 */
+	public static Activator getDefault() {
+		return plugin;
+	}
+
+	/**
+	 * Returns an image descriptor for the image file at the given plug-in
+	 * relative path
+	 * 
+	 * @param path
+	 *          the path
+	 * @return the image descriptor
+	 */
+	public static ImageDescriptor getImageDescriptor(String path) {
+		return imageDescriptorFromPlugin(PLUGIN_ID, path);
+	}
+
+	/**
+	 * Loads a file from the bundle/plug-in.
+	 * 
+	 * @param path
+	 *          relative path to the file
+	 * @return input stream of the required file
+	 * @throws IOException
+	 *           is thrown, if I/O exception occurs
+	 * @throws IllegalArgumentException
+	 *           is thrown, if path is null or empty string (after being trimmed)
+	 */
+	public static InputStream loadFile(String path) throws IOException {
+		if (path == null || path.trim().length() == 0)
+			throw new IllegalArgumentException("path can not be null");
+
+		return FileLocator.openStream(Activator.getDefault().getBundle(), new Path(path), false);
+	}
+
+	public static void log(String msg) {
+		log(msg, null);
+	}
+
+	public static void log(String msg, Exception e) {
+		logger.log(new Status(Status.INFO, PLUGIN_ID, Status.OK, msg, e));
+	}
+
+}
---
+++ tags/1.1.2/src/eu/musoft/eclipse/xpath/evaluation/plugin/views/listeners/EvaluationJob.java
@@ -0,0 +1,98 @@
+/**
+ * Copyright (c) 2011, Martin Uhlir
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * The name of the author may not be used to endorse or promote
+ *       products derived from this software without specific prior written
+ *       permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ''AS IS'' AND ANY EXPRESS
+ * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package eu.musoft.eclipse.xpath.evaluation.plugin.views.listeners;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.Job;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Text;
+
+import eu.musoft.eclipse.xpath.evaluation.plugin.Activator;
+import eu.musoft.eclipse.xpath.evaluation.plugin.XPathEvaluator;
+
+/**
+ * This class performs actual XPath evaluation. The task will run in a new
+ * thread while invoking the 'schedule()' method on this class instance.
+ */
+class EvaluationJob extends Job {
+
+	private Text result;
+
+	private String xpath;
+	private String xml;
+	private boolean isPrettyPrint;
+
+	/**
+	 * Constructor.
+	 * 
+	 * @param xpath
+	 *          XPath to be evaluated
+	 * @param xml
+	 *          XML to evaluate the XPath expression on
+	 * @param result
+	 *          evaluated subset of the original XML
+	 */
+	public EvaluationJob(final String xpath, final String xml, final boolean isPrettyPrint, final Text result) {
+		super("XPath evaluation");
+
+		this.result = result;
+		this.xpath = xpath;
+		this.xml = xml;
+		this.isPrettyPrint = isPrettyPrint;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.eclipse.core.runtime.jobs.Job#run(org.eclipse.core.runtime.IProgressMonitor
+	 * )
+	 */
+	@Override
+	protected IStatus run(IProgressMonitor monitor) {
+		try {
+			String evaluatedResult = XPathEvaluator.evaluate(xpath, xml, isPrettyPrint);
+			outputResult(evaluatedResult);
+		} catch (Exception e) {
+			Status errorStatus = new Status(IStatus.ERROR, Activator.PLUGIN_ID, e.getMessage(), e.getCause());
+			return errorStatus;
+		}
+
+		return Status.OK_STATUS;
+	}
+
+	private void outputResult(final String evaluatedResult) {
+		Display.getDefault().syncExec(new Runnable() {
+			public void run() {
+				result.setText(evaluatedResult);
+			}
+		});
+	}
+}
