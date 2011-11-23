7f39ee0c5da0d1526a9073980ea60ba5ec50c66b
stoupa91@gmail.com <stoupa91@gmail.com@9130e583-ed78-ccbd-5243-a5cfb3686d26>
2011-11-23 00:42:24 +0000

    Added namespaces tab.

---
+++ trunk/eclipse-xpath-evaluation-plugin/src/eu/musoft/eclipse/xpath/evaluation/plugin/views/namespaces/ContentProvider.java
@@ -0,0 +1,60 @@
+/**
+ * Copyright (c) 2011, Martin Uhlir
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * The name of the author may not be used to endorse or promote
+ *       products derived from this software without specific prior written
+ *       permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ''AS IS'' AND ANY EXPRESS
+ * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package eu.musoft.eclipse.xpath.evaluation.plugin.views.namespaces;
+
+import java.util.List;
+
+import org.eclipse.jface.viewers.IStructuredContentProvider;
+import org.eclipse.jface.viewers.Viewer;
+
+/**
+ * This class provides the content for the namespaces table.
+ */
+class ContentProvider implements IStructuredContentProvider {
+
+	/**
+	 * Returns the Prefix objects
+	 */
+	public Object[] getElements(Object inputElement) {
+		return ((List<Namespace>) inputElement).toArray();
+	}
+
+	/**
+	 * Disposes any created resources.
+	 */
+	public void dispose() {
+		// Do nothing
+	}
+
+	/**
+	 * Called when the input changes.
+	 */
+	public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {
+		// Ignore
+	}
+}
